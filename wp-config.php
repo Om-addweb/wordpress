f10cec7c15cdea419b7900a93a9eec32fce9a4cc
Om <om@addwebsolution.in>
2026-09-22 13:33:45 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'wordpress_db' );
 
 /** Database username */
-define( 'DB_USER', 'om' );
+define( 'DB_USER', 'omp' );
 
 /** Database password */
 define( 'DB_PASSWORD', 'AddWeb_2026!#' );
